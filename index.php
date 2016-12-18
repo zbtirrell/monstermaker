f919412683b19b5cc617a983a17a7ea49c16944d
Zach Tirrell <zbtirrell@gmail.com>
2016-12-17 22:13:09 -0500

    add an icon

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<title>Parker and Xander's Monster Maker</title>
 	<meta name="apple-mobile-web-app-capable" content="yes">
+	<link rel="apple-touch-icon" href="/icon.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<link media="all" rel="stylesheet" href="style.css" />
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<script type="text/javascript" src="main.js"></script>​
