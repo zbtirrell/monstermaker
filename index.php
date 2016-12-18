4323d82386ebe3ececb4d4106626ffe3bf3e45c6
Zach Tirrell <zbtirrell@gmail.com>
2016-12-17 22:08:12 -0500

    making iOS happy

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Parker and Xander's Monster Maker</title>
+	<meta name="apple-mobile-web-app-capable" content="yes">
 	<link media="all" rel="stylesheet" href="style.css" />
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<script type="text/javascript" src="main.js"></script>​
